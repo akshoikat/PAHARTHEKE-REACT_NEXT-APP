e8a467c3371e28d539c2a599436c2d72360cf350
NooB-AK-47 <azariyan005@gmail.com>
2026-04-04 05:58:53 +0600

    update

--- src/lib/data.js
+++ src/lib/data.js
@@ -21,6 +21,88 @@ export const getCategories = async (search = "") => {
   }
 };
 
+import React from "react";
+import axios from "axios";
+
+
+// export const getCategories = async (search = "") => {
+//   try {
+//     const res = await fetch(
+//       `https://pahartheke.com/api/v6/products`
+//     );
+
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch categories");
+//     }
+
+//     const data = await res.json();
+
+//     return data.data.data; // because Laravel pagination wraps inside data
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// };
+
+
+
+
+
+
+
+// import React from "react";
+// import axios from "axios";
+
+
+// export const getCategories = async (search = "") => {
+//   try {
+//     const res = await fetch(
+//       `https://pahartheke.com/api/v6/products`
+//     );
+
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch categories");
+//     }
+
+//     const data = await res.json();
+
+//     return data.data.data; // because Laravel pagination wraps inside data
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// };
+
+// import React from "react";
+// import axios from "axios";
+
+
+// export const getCategories = async (search = "") => {
+//   try {
+//     const res = await fetch(
+//       `https://pahartheke.com/api/v6/products`
+//     );
+
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch categories");
+//     }
+
+//     const data = await res.json();
+
+//     return data.data.data; // because Laravel pagination wraps inside data
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// };
+
+
+
+
+
+
+
+
 
 
 
